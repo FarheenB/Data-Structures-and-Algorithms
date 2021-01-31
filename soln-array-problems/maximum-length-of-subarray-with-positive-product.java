183a0a6d59ca4a209e408d692d81e86460034041
FarheenB <farheenbano94@gmail.com>
2021-01-31 23:20:26 +0530

    Uploaded JAVA Solutions

---
+++ soln-array-problems/maximum-length-of-subarray-with-positive-product.java
@@ -0,0 +1,29 @@
+/*
+https://www.geeksforgeeks.org/length-of-longest-subarray-with-positive-product/
+https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+*/
+
+class Solution {
+    public int getMaxLen(int[] nums) {
+        int count_pos=0;
+        int count_neg=0;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+           if(num == 0)  {
+                count_pos = 0;
+                count_neg = 0;
+            }
+            else if(num > 0) {
+                count_pos++;
+                count_neg = count_neg == 0 ? 0  : count_neg+1;
+            }
+            else {
+                int temp = count_pos;
+                count_pos = count_neg == 0 ? 0  : count_neg+1;
+                count_neg = temp+1;
+            }
+            max = Math.max(max, count_pos);
+        }
+        return max;
+    }
+}
---
+++ soln-string-problems/print-longest-substring-without-repeating-chars.java
@@ -0,0 +1,45 @@
+/*
+https://www.geeksforgeeks.org/print-longest-substring-without-repeating-characters/
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution{
+    String longestUniqueSubsttr(String s){
+        char[] str=s.toCharArray();
+	    int i=0;
+	    int j=0;
+	    int max=Integer.MIN_VALUE;
+	    
+	    HashMap<Character,Integer> map=new HashMap<>();
+	    String result="";
+	    while(j<str.length){
+	        map.put(str[j],map.getOrDefault(str[j],0)+1);
+            if(map.get(str[j])==1){
+                if(max<j-i+1){
+                    max=j-i+1;
+                    result=s.substring(i,j+1);                   
+                }
+	            j++;
+	        }  
+            else if(map.get(str[j])>1){
+                while(map.get(str[j])>1 && i<str.length){
+    	            if(map.containsKey(str[i])){
+        	            if(map.get(str[i])>0){
+        	                map.put(str[i],map.get(str[i])-1);
+        	            }
+        	            else{
+        	                map.remove(str[i]);
+        	            }
+    	            }
+    	            i++;
+    	        }
+    	        j++;
+            }
+	    }
+	   
+	    return result;
+	}
+}
