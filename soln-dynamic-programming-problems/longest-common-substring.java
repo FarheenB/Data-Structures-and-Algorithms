6fff6b51da0ffe68ed82041674da7a36a43f4bf7
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-14 21:18:40 +0530

    Update longest-common-substring.java

--- soln-dynamic-programming-problems/longest-common-substring.java
+++ soln-dynamic-programming-problems/longest-common-substring.java
@@ -1,5 +1,8 @@
 /*
-https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1
+ @author Farheen Bano
+  
+ Reference-
+ https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1
 */
 
 import java.util.*;
