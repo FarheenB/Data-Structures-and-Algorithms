175231c4366e6d37a2f3584b540414bd14151c39
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-14 16:58:17 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.java

--- soln-dynamic-programming-problems/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ soln-dynamic-programming-problems/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,5 +1,8 @@
 /*
-https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 */
 
 import java.util.*;
@@ -8,7 +11,6 @@ import java.io.*;
 
 class Solution {
   public int maxProfit(int[] prices) {
-
     int sold = Integer.MIN_VALUE, inHand = Integer.MIN_VALUE, reset = 0;
 
     for (int priceToday : prices) {
@@ -18,7 +20,6 @@ class Solution {
       inHand = Math.max(inHand, reset - priceToday);
       reset = Math.max(reset, lastSold);
     }
-
     return Math.max(sold, reset);
   }
 }
