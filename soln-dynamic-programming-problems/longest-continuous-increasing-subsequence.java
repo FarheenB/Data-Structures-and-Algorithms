60c3f0319ef88fc590dfcebd8020af3d543002d4
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-14 21:18:46 +0530

    Update longest-continuous-increasing-subsequence.java

--- soln-dynamic-programming-problems/longest-continuous-increasing-subsequence.java
+++ soln-dynamic-programming-problems/longest-continuous-increasing-subsequence.java
@@ -1,5 +1,8 @@
 /*
-https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/longest-continuous-increasing-subsequence/
 */
 
 import java.util.*;
