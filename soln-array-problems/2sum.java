78f0f3d11f014e6b20ce4a6afadaaede8ecf3b25
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 20:33:01 +0530

    Update 2sum.java

--- soln-array-problems/2sum.java
+++ soln-array-problems/2sum.java
@@ -1,5 +1,8 @@
 /*
-https://leetcode.com/problems/two-sum/
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/two-sum/
 */
 
 import java.util.*;
@@ -7,23 +10,19 @@ import java.lang.*;
 import java.io.*;
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) 
-    {
-        HashMap< Integer,Integer> second=new HashMap<Integer,Integer>();
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> second=new HashMap<Integer,Integer>();
         int result[]=new int[2];
         int other;
-        for(int i=0;i<nums.length;i++)
-        {
+        for(int i=0;i<nums.length;i++) {
             other=target-nums[i];
-            if(second.containsKey(nums[i]))
-            {
+            if(second.containsKey(nums[i])) {
                 result[0]=second.get(nums[i]);
                 result[1]=i;
                 return result;
             }
             second.put(other,i);
         }
-        return result;
-      
+        return result;      
     }
 }
