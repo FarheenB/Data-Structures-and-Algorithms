34e15df6cb2d43737d7d1ddf647db66736e32093
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 20:30:11 +0530

    Update min-deletion-to-make-two-strings-same.java

--- soln-dynamic-programming-problems/min-deletion-to-make-two-strings-same.java
+++ soln-dynamic-programming-problems/min-deletion-to-make-two-strings-same.java
@@ -1,5 +1,8 @@
 /*
-https://leetcode.com/problems/delete-operation-for-two-strings/
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/delete-operation-for-two-strings/
 */
 
 import java.util.*;
