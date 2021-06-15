b92fb5a650676cbcbf16f96f228a3656a47adaa4
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 20:41:11 +0530

    Update can-place-flowers.java

--- soln-array-problems/can-place-flowers.java
+++ soln-array-problems/can-place-flowers.java
@@ -1,5 +1,8 @@
 /*
-https://leetcode.com/problems/can-place-flowers/
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/can-place-flowers/
 */
 
 import java.util.*;
@@ -7,8 +10,7 @@ import java.lang.*;
 import java.io.*;
 
 class Solution {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {        
         if(flowerbed.length==0){
             return false; 
         }
@@ -17,24 +19,20 @@ class Solution {
         if(flowerbed.length==1){
             return (flowerbed[0]==0 && n==1) ?true:false; 
         }
+        
         for(int i=0;i<flowerbed.length;i++){
-            if(i==0 && flowerbed[i]==0 && flowerbed[i+1]==0 && n>0)
-            {
+            if(i==0 && flowerbed[i]==0 && flowerbed[i+1]==0 && n>0) {
                 flowerbed[i]=1;
                 n--;
-            }    
-               
-            else if(i==flowerbed.length-1 && flowerbed[i]==0 && flowerbed[i-1]==0 && n>0)
-            {
+            }                   
+            else if(i==flowerbed.length-1 && flowerbed[i]==0 && flowerbed[i-1]==0 && n>0) {
                 flowerbed[i]=1;
                 n--;
             }
-            else if(i>0 && i<flowerbed.length-1 && flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0 && n>0)
-            {
+            else if(i>0 && i<flowerbed.length-1 && flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0 && n>0) {
                 flowerbed[i]=1;
                 n--;
-            }
-            
+            }            
         }
         if(n==0){
             return true;
