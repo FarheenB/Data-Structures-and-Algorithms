3922a02f1f8b2c150ab19953a580f06a10fcef6f
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 19:59:09 +0530

    Update n-th-tribonacci-number.java

--- soln-dynamic-programming-problems/n-th-tribonacci-number.java
+++ soln-dynamic-programming-problems/n-th-tribonacci-number.java
@@ -1,6 +1,9 @@
 /*
-https://leetcode.com/problems/n-th-tribonacci-number/
-https://www.geeksforgeeks.org/tribonacci-numbers/
+ @author Farheen Bano
+  
+ References-
+ https://leetcode.com/problems/n-th-tribonacci-number/
+ https://www.geeksforgeeks.org/tribonacci-numbers/
 */
 
 import java.io.*;
@@ -19,12 +22,9 @@ class Solution {
             return f[n]; 
         f[2] = 1; 
 
-        for (i = 3; i <= n; i++) 
-        { 
+        for (i = 3; i <= n; i++) { 
             f[i] = f[i-1] + f[i-2]+ f[i-3];; 
         } 
-
         return f[n]; 
-    
     }
 }
