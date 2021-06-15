8aa40db40403c6811af90ca9541e634f9c7a2b7d
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 21:30:46 +0530

    Update burst-balloons.java

--- soln-dynamic-programming-problems/burst-balloons.java
+++ soln-dynamic-programming-problems/burst-balloons.java
@@ -1,6 +1,15 @@
 /*
  @author Farheen Bano
   
+ Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented
+ by array nums. You are asked to burst all the balloons. If the you burst balloon i you will
+ get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst,
+ the left and right then becomes adjacent.
+ Find the maximum coins you can collect by bursting the balloons wisely.
+ 
+ Time complexity O(n^3)
+ Space complexity O(n^2)
+ 
  Reference-
  https://leetcode.com/problems/burst-balloons/
 
