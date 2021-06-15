f2d9f78abe40390c16282e14067c4479bb96c22f
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-15 19:57:51 +0530

    Update min-cost-climbing-stairs.java

--- soln-dynamic-programming-problems/min-cost-climbing-stairs.java
+++ soln-dynamic-programming-problems/min-cost-climbing-stairs.java
@@ -1,7 +1,12 @@
 /*
-https://leetcode.com/problems/min-cost-climbing-stairs/
+ @author Farheen Bano
+  
+ Each time you can either climb 1 or 2 steps. Return the minimum cost to reach the top of the floor.
+ 
+ Reference-
+ https://leetcode.com/problems/min-cost-climbing-stairs/
 
-Note: either climb 1 or 2 steps
+ NOTE: Variation of Fibinacci Series
 */
 
 import java.io.*;
@@ -9,13 +14,11 @@ import java.util.*;
 import java.lang.*;
 
 class Solution {
-
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int[cost.length];
         dp[0] = cost[0];
         dp[1] = cost[1];
-        for(int i=2;i<dp.length;i++)
-        {
+        for(int i=2;i<dp.length;i++) {
             dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
         } 
         return Math.min(dp[cost.length-1],dp[cost.length-2]);
