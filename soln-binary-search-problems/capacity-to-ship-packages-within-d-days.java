36679d7beb608dd21a8bac5e9884bed0039815ab
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 16:58:25 +0530

    Update capacity-to-ship-packages-within-d-days.java

--- soln-binary-search-problems/capacity-to-ship-packages-within-d-days.java
+++ soln-binary-search-problems/capacity-to-ship-packages-within-d-days.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 */
 
 import java.util.*;
