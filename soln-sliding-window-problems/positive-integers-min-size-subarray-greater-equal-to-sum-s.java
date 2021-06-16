6d038421f6838510bd105ed1d4cc866b269fcf98
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 17:54:21 +0530

    Update positive-integers-min-size-subarray-greater-equal-to-sum-s.java

--- soln-sliding-window-problems/positive-integers-min-size-subarray-greater-equal-to-sum-s.java
+++ soln-sliding-window-problems/positive-integers-min-size-subarray-greater-equal-to-sum-s.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/minimum-size-subarray-sum/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/minimum-size-subarray-sum/
 */
 
 class Solution {
