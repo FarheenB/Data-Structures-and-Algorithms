5b0d79c5e20f0427066949276b3b94d6bb89deb8
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 20:57:41 +0530

    Update maximum-height-binary-tree.java

--- soln-tree-problems/maximum-height-binary-tree.java
+++ soln-tree-problems/maximum-height-binary-tree.java
@@ -1,5 +1,8 @@
-/*
-https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
 */
 
 import java.util.*;
