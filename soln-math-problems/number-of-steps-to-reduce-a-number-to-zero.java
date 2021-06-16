a197ac2423dfa4748ab84088fbb57783329b779d
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 18:09:30 +0530

    Update number-of-steps-to-reduce-a-number-to-zero.java

--- soln-math-problems/number-of-steps-to-reduce-a-number-to-zero.java
+++ soln-math-problems/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
 */
 
 import java.util.*;
