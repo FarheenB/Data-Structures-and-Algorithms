01fca772452f1f4a35b7d947dc90e46e36a81fa3
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 17:54:35 +0530

    Update positive-integers-subarray-with-given-sum-s.java

--- soln-sliding-window-problems/positive-integers-subarray-with-given-sum-s.java
+++ soln-sliding-window-problems/positive-integers-subarray-with-given-sum-s.java
@@ -1,5 +1,8 @@
-/*
-https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
 */
 
 class Subarray{
