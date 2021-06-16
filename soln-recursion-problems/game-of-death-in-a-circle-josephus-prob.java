260817b4fa31c7c4c3e734f3941dbeff22ad80d6
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 18:08:05 +0530

    Update game-of-death-in-a-circle-josephus-prob.java

--- soln-recursion-problems/game-of-death-in-a-circle-josephus-prob.java
+++ soln-recursion-problems/game-of-death-in-a-circle-josephus-prob.java
@@ -1,5 +1,8 @@
-/*
-https://practice.geeksforgeeks.org/problems/game-of-death-in-a-circle/0#
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://practice.geeksforgeeks.org/problems/game-of-death-in-a-circle/0#
 */
 
 import java.util.*;
