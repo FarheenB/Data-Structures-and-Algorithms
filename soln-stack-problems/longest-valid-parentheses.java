91033dbcff8bd1302c755f7010e963819a21b10c
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:34:25 +0530

    Update longest-valid-parentheses.java

--- soln-stack-problems/longest-valid-parentheses.java
+++ soln-stack-problems/longest-valid-parentheses.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/longest-valid-parentheses/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/longest-valid-parentheses/
 */
 
 import java.util.*;
