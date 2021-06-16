dfbdd8b28aa0de5ed6b24c1719555c88a573bb1c
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:28:22 +0530

    Update validate-stack-sequences.java

--- soln-stack-problems/validate-stack-sequences.java
+++ soln-stack-problems/validate-stack-sequences.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/validate-stack-sequences/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/validate-stack-sequences/
 */
 
 import java.util.*;
