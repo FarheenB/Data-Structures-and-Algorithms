3fe9b2e4a57fdb166e9cfabd9dc1252d80c3eecc
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:17:14 +0530

    Update longest-common-prefix.java

--- soln-string-problems/longest-common-prefix.java
+++ soln-string-problems/longest-common-prefix.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/longest-common-prefix/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/longest-common-prefix/
 */
 
 import java.util.*;
