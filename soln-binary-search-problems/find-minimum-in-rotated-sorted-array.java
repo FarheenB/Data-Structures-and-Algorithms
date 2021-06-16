4bec0e6506928f6cc5415465b0dde6457444a0a6
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 16:54:00 +0530

    Update find-minimum-in-rotated-sorted-array.java

--- soln-binary-search-problems/find-minimum-in-rotated-sorted-array.java
+++ soln-binary-search-problems/find-minimum-in-rotated-sorted-array.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
 */
 
 import java.util.*;
