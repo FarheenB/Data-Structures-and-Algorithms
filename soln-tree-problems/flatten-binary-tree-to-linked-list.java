1de2cde29b6205df6c89bbc6f7069f352be2202b
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 20:50:35 +0530

    Update flatten-binary-tree-to-linked-list.java

--- soln-tree-problems/flatten-binary-tree-to-linked-list.java
+++ soln-tree-problems/flatten-binary-tree-to-linked-list.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 */
 
 import java.util.*;
