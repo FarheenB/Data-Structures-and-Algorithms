cb4e53195b1a88c9b1212d86dda1c19133b2c597
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:13:32 +0530

    Update palindrome-permutation.java

--- soln-string-problems/palindrome-permutation.java
+++ soln-string-problems/palindrome-permutation.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/palindrome-permutation/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/palindrome-permutation/
 */
 
 import java.util.*;
