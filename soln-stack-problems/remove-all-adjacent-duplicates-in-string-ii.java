15d0e43fcec2cfcc8990ec83c77d39f0199640bc
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:26:18 +0530

    Update remove-all-adjacent-duplicates-in-string-ii.java

--- soln-stack-problems/remove-all-adjacent-duplicates-in-string-ii.java
+++ soln-stack-problems/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii
 */
 
 import java.util.*;
