48621dab263e4b38045138fb40ba8ddc684a1bab
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 18:22:18 +0530

    Update search-in-almost-sorted-array.java

--- soln-binary-search-problems/search-in-almost-sorted-array.java
+++ soln-binary-search-problems/search-in-almost-sorted-array.java
@@ -1,16 +1,17 @@
-/*
-https://www.geeksforgeeks.org/search-almost-sorted-array/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://www.geeksforgeeks.org/search-almost-sorted-array/
 */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution
-{
-	public int binarySearch(int[] nums, int start, int end, int target){ 
-        while(start<=end)
-        {
+class Solution {
+	public int binarySearch(int[] nums, int start, int end, int target) { 
+        while(start<=end) {
             int mid=start+(end-start)/2;
             if(target==nums[mid])
                 return mid;
