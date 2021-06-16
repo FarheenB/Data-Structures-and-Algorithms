d57168c0b21e716550188994060e2983ca90688d
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 17:11:56 +0530

    Update k-pairs-with-smallest-sums.java

--- soln-heap-problems/k-pairs-with-smallest-sums.java
+++ soln-heap-problems/k-pairs-with-smallest-sums.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
 */
 
 import java.util.*;
