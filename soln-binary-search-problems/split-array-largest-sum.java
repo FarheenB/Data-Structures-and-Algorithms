463754b60b3ce4b538beb1646b27a3a374f0444d
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 17:06:14 +0530

    Update split-array-largest-sum.java

--- soln-binary-search-problems/split-array-largest-sum.java
+++ soln-binary-search-problems/split-array-largest-sum.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/split-array-largest-sum/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/split-array-largest-sum/
 */
 
 class Solution {
