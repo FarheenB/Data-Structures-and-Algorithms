502a1921beca89e5a6b79eed4fc8014224bccf89
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:31:35 +0530

    Update reverse-a-stack-using-recursion.java

--- soln-stack-problems/reverse-a-stack-using-recursion.java
+++ soln-stack-problems/reverse-a-stack-using-recursion.java
@@ -1,5 +1,8 @@
-/*
-https://www.geeksforgeeks.org/reverse-a-stack-using-recursion/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://www.geeksforgeeks.org/reverse-a-stack-using-recursion/
 */
 
 import java.util.*;
@@ -8,22 +11,22 @@ import java.io.*;
 
 class Solution {
     static Stack<Character> stack=new Stack<>();
-	public static void reverseStack(){
-	    if(stack.isEmpty())
-	        return;
+    public static void reverseStack(){
+        if(stack.isEmpty())
+	    return;
 	        
-	    char n=stack.pop();
-	    reverseStack();
-	    insertAtBottom(n);  
-	}
+	char n=stack.pop();
+	reverseStack();
+	insertAtBottom(n);  
+    }
 	
-	public static void insertAtBottom(char n){
-	    if(stack.isEmpty())
-	        stack.push(n);
-	    else{
-	        char x=stack.pop();
-	        insertAtBottom(n);
-	        stack.push(x);
-	    }
-	}
+    public static void insertAtBottom(char n){
+        if(stack.isEmpty())
+ 	    stack.push(n);
+	else{
+	    char x=stack.pop();
+	    insertAtBottom(n);
+	    stack.push(x);
+        }
+    }
 }
