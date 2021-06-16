d5f3816cdf9b6d84cf7737d59d65e3599f47d7e9
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 21:47:33 +0530

    Update view-vertical-traversal-of-a-binary-tree.java

--- soln-tree-problems/view-vertical-traversal-of-a-binary-tree.java
+++ soln-tree-problems/view-vertical-traversal-of-a-binary-tree.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 */
 
 /**
