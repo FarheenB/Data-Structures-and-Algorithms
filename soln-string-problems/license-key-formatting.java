28464f4929ff42ad5c223f5b91bcf9996aa59da2
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:17:06 +0530

    Update license-key-formatting.java

--- soln-string-problems/license-key-formatting.java
+++ soln-string-problems/license-key-formatting.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/license-key-formatting
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/license-key-formatting
 */
 
 import java.util.*;
