53e59528dc3b1456cdef66d1df8531bc5be3adc5
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 17:03:56 +0530

    Update search-in-sorted-array-of-unknown-size.java

--- soln-binary-search-problems/search-in-sorted-array-of-unknown-size.java
+++ soln-binary-search-problems/search-in-sorted-array-of-unknown-size.java
@@ -1,6 +1,9 @@
-/*
-https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
-https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
+/**
+ @author Farheen Bano
+  
+ References-
+ https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+ https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
 */
 
 import java.util.*;
