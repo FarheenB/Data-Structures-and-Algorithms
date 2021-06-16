0a682200e0f865a224944393d03f3d2e44f9b50f
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 20:45:38 +0530

    Update binary-tree-longest-consecutive-sequence.java

--- soln-tree-problems/binary-tree-longest-consecutive-sequence.java
+++ soln-tree-problems/binary-tree-longest-consecutive-sequence.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/binary-tree-longest-consecutive-sequence
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/binary-tree-longest-consecutive-sequence
 */
 
 import java.util.*;
