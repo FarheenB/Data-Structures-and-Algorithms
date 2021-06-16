a915f4d93b7ef760deebc455f1104f68d31dbecd
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 16:53:15 +0530

    Update find-single-element-in-a-sorted-array.java

--- soln-binary-search-problems/find-single-element-in-a-sorted-array.java
+++ soln-binary-search-problems/find-single-element-in-a-sorted-array.java
@@ -1,5 +1,8 @@
-/*
-https://leetcode.com/problems/single-element-in-a-sorted-array/
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://leetcode.com/problems/single-element-in-a-sorted-array/
 */
 
 import java.util.*;
