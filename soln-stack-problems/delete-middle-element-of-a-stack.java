5fd16a62395958bf28c528c88373aaca919f5f12
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-06-16 22:33:20 +0530

    Update delete-middle-element-of-a-stack.java

--- soln-stack-problems/delete-middle-element-of-a-stack.java
+++ soln-stack-problems/delete-middle-element-of-a-stack.java
@@ -1,5 +1,8 @@
-/*
-https://practice.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1
+/**
+ @author Farheen Bano
+  
+ Reference-
+ https://practice.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1
 */
 
 import java.util.*;
