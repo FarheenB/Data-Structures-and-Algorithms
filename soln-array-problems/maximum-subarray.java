62affb5fd9fdd3e201bb4db8243d6875e99a2fe2
Farheen Bano <59408650+FarheenB@users.noreply.github.com>
2021-07-31 16:05:26 +0530

    Update maximum-subarray.java

--- soln-array-problems/maximum-subarray.java
+++ soln-array-problems/maximum-subarray.java
@@ -16,17 +16,14 @@ class Solution {
         int max_so_far = Integer.MIN_VALUE, 
         max_ending_here = 0,start = 0, end = 0, s = 0; 
   
-        for (int i = 0; i < size; i++)  
-        { 
+        for (int i = 0; i < size; i++){ 
             max_ending_here += a[i]; 
-            if (max_ending_here < 0)  
-            { 
+            if (max_ending_here < 0){ 
                 max_ending_here = 0; 
                 s = i + 1; 
             } 
   
-            if (max_so_far < max_ending_here)  
-            { 
+            if (max_so_far < max_ending_here){ 
                 max_so_far = max_ending_here; 
                 start = s; 
                 end = i; 
