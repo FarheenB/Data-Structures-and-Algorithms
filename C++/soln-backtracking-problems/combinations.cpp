ffad1cff139cb89f520479e59ee4a51b43802a13
FarheenB <farheenbano94@gmail.com>
2021-08-18 22:41:40 +0530

    Updated C++ Solution

--- C++/soln-backtracking-problems/combinations.cpp
+++ C++/soln-backtracking-problems/combinations.cpp
@@ -10,24 +10,22 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-         vector<vector<int>> result;
+        vector<vector<int>> result;
         vector<int> cur;
         possibleCombination(n,1,k,cur,result);
         return result;
     }    
     
-    void possibleCombination(int n, int start, int remaining, vector<int>& cur, vector<vector<int>>& result){
-        if(remaining==0)
+    void possibleCombination(int n, int index, int remaining, vector<int>& cur, vector<vector<int>>& result){
+        if(remaining==0){
             result.push_back(cur);
-        
-        else{
-            for(int i=start;i<=n-remaining+1;i++){
-                if(find(cur.begin(),cur.end(),i)!=cur.end())
-                    continue;
-                cur.push_back(i);
-                possibleCombination(n,i+1,remaining-1,cur,result);
-                cur.pop_back();
-            }
+            return;
         }
+                
+        for(int i=index;i<=n-remaining+1;i++){
+            cur.push_back(i);
+            possibleCombination(n,i+1,remaining-1,cur,result);
+            cur.pop_back();
+        }        
     }    
 };
