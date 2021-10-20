8ba5e1e361a123e97963cf6df1e10a3f35c10435
FarheenB <farheenbano94@gmail.com>
2021-10-20 21:46:08 +0530

    Updated C++ solution

--- C++/soln-grid-problems/DFS-number-of-islands.cpp
+++ C++/soln-grid-problems/DFS-number-of-islands.cpp
@@ -10,30 +10,29 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int islandCount=0;
-        
+        int numberIslands=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
-                    islandCount+=dfs(grid,i,j);                    
+                    grid[i][j]='0';
+                    numberIslands++;
+                    dfs(grid,i,j);  
                 }
             }
         }
-        return islandCount;
+        return numberIslands;        
     }
     
-    vector<vector<int>> dirs={{1,0},{0,1},{-1,0},{0,-1}};
-    int dfs(vector<vector<char>>& grid,int i, int j){
+    int dirs[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
+    
+    void dfs(vector<vector<char>>& grid, int i, int j){        
         for(auto dir:dirs){
             int dir_x=i+dir[0];
             int dir_y=j+dir[1];
-            
-            if(dir_x>=0 && dir_y>=0 && dir_x<grid.size() && dir_y<grid[0].size() && grid[dir_x][dir_y]!='0'){
+            if(dir_x>=0 && dir_x<grid.size() && dir_y>=0 && dir_y<grid[i].size() && grid[dir_x][dir_y]!='0'){
                 grid[dir_x][dir_y]='0';
-                dfs(grid,dir_x, dir_y);
+                dfs(grid,dir_x,dir_y);         
             }
-
         }
-        return 1;
     }
 };
